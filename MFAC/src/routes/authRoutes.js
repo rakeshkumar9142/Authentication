434bce3a5e778c33a6890d0e94d95dc7ca7c996e
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-06-13 09:31:22 +0500

    ABAC Added

--- MFAC/src/routes/authRoutes.js
+++ MFAC/src/routes/authRoutes.js
@@ -7,7 +7,7 @@ import { register,
     setup2FA,
     verify2FA,
     reset2FA }
- from "../../src/controllers/authController.js";
+ from "../controllers/authController.js";
 
 const router = Router();
 
