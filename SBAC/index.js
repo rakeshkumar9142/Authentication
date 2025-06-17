c2ead044db6580cdf0096cefd7ca444732baab2a
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-06-17 07:39:12 +0500

    Now This is working with basic login and we are able to redirect to the another route

--- SBAC/index.js
+++ SBAC/index.js
@@ -1,27 +1,33 @@
 const express = require('express');
-
+const session = require('express-session')
 const app = express();
 
 app.set('view engine','ejs');
 
 app.use(express.urlencoded({extended : false}))
 
+app.use(session({
+    secret : 'my-sesion-secret',
+    resave : true,
+    saveUninitialized : false,
+}))
+
 app.get('/',(req,res) => {
     res.render('home');
 })
 
 app.get('/login',(req,res) => {
-    res.render('login')
+    res.render('login',{error : null})
 })
 
 app.post('/login',(req,res) => {
     if(req.body.name === "john" && req.body.password === "123") {
-        // Create sesion and starts user logged details
-    } else {
+     req.session.user = {id:1,name : 'john',sname : 'John Doe'}
+     res.redirect('/');
+       } else {
         res.render('login',{error : "wrong credentials"})
     }
 })
-
 app.listen(3000,() => {
     console.log('Server is runing on PORT : 3000');
 })
