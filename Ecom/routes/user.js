89ccd8af0e7a02439c1471f4005be9b92989c945
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-06-22 06:16:40 +0500

    Something is wrong

--- Ecom/routes/user.js
+++ Ecom/routes/user.js
@@ -1,5 +1,24 @@
+const { verifyToekn, verifyTokenAuthorization } = require('./verifyToken');
 const router = require('express').Router();
+const User = require('../models/user')
 
-router.put('/:id',)
+router.put('/:id',verifyTokenAuthorization,async(req,res) => {
+   if (req.body.password) {
+    req.body.password = CryptJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+    ).toString();
+   }
+
+   try{
+    const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+        $set: req.body
+    },{new : true});
+    return res.status(200).json({updatedUser});
+   } catch(err) {
+    return res.status(500).json({err})
+   }
+
+})
 
 module.exports = router;
--- Ecom/routes/verifyToken.js
+++ Ecom/routes/verifyToken.js
@@ -1,5 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToekn = (req,res,next) => {
+  const authHeader = req.headers.token
+  if (authHeader) {
+    jwt.verify(token,process.env.JWT_SEC),(err,user) => {
+        if(err) return res.status(403).json("Token is not valid!");
+        req.user = user
+        next();
+    }
+  } else {
+    return res.status(401).json({message : "you are not authenticated"})
+  }
+};
 
+const verifyTokenAuthorization = (req,res,next) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+    } else {
+        return res.status(403).json({message : "you are not allowed to do that"})
+    }
 }
+
+module.exports = {verifyToekn,verifyTokenAuthorization};
