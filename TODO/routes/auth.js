107b91c8e242ed34a665cd84820ed1d061ef8636
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-07-17 05:46:21 +0500

    MFAC code reuse for the same funcanality

--- TODO/routes/auth.js
+++ TODO/routes/auth.js
@@ -1,7 +1,8 @@
 
-
+const passport = require('passport');
 const {register,login} = require('../controllers/authController.js');
 const router = express.Router();
 
 router.post('/register',register);
 router.post('/login',login);
+
